6dfabeb0d6850e200983d9f03b4b6ffdb4277e9d
Mohamed Essam <ana3ssam2017@gmail.com>
2019-02-26 12:55:07 +0200

    Enhancing POST Method to return correct HTTP Status Code and Location.

--- src/main/java/mo/springrestful/controllers/UserController.java
+++ src/main/java/mo/springrestful/controllers/UserController.java
@@ -3,8 +3,11 @@ package mo.springrestful.controllers;
 import mo.springrestful.models.User;
 import mo.springrestful.services.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,8 +29,16 @@ public class UserController {
 
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        userDaoService.save(user);
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User save = userDaoService.save(user);
+
+        URI Location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(save.getId())
+                .toUri();
+
+        return ResponseEntity.created(Location).build();
     }
 
 }
